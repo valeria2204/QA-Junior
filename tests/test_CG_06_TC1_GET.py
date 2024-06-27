3117a04abcc6763b436fe9d21bd6ba4eb93eb73e
shizukani9 <tatianamayorga9999@gmail.com>
2024-06-27 12:31:03 -0400

    archivo test_CG_06_TC1_GET Caso de prueba 1: 200 Success ID válido de un grupo de clientes existente

---
+++ tests/test_CG_06_TC1_GET.py
@@ -0,0 +1,30 @@
+import pytest
+import requests
+
+# URL base de la API
+base_url = "https://magento2-demo.magebit.com/rest/default/V1/customerGroups"
+# Token de acceso (reemplaza 'your_access_token' con tu token válido)
+access_token = "psz0zk8oqeetwpgt5i0x91a1jprqfgch"
+
+
+# Caso de prueba 1: 200 Success
+def test_get_customer_group_success():
+    group_id = 1  # ID válido de un grupo de clientes existente
+    url = f"{base_url}/{group_id}"
+
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    response_data = response.json()
+
+    assert response.status_code == 200
+    assert "id" in response_data
+    assert "code" in response_data
+    assert "tax_class_id" in response_data
+    assert "tax_class_name" in response_data
+    # Verifica que 'extension_attributes' esté presente en caso de que exista
+    if "extension_attributes" in response_data:
+        assert response_data["extension_attributes"] is not None
