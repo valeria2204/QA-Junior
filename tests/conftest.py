a910a3a3f8e53df0b6b4dbbd6c7e4a9e86773841
Jorge Flores Chipana <201400892@est.umss.edu>
2024-06-27 21:38:12 -0400

    C-2-TC7: [POST] Validar Schema - Crear nueva cuenta 'Customer' solo con la informacion valida requerida

---
+++ tests/conftest.py
@@ -0,0 +1,41 @@
+import json
+
+import pytest
+import requests
+
+from src.singleton import Singleton
+
+
+@pytest.fixture
+def get_token_login():
+    url = Singleton.get_base_url() + "/rest/default/V1/integration/admin/token"
+    headers = {'Content-Type': 'application/json'}
+    payload = json.dumps({
+        "username": Singleton.get_username(),
+        "password": Singleton.get_password()
+    })
+    response = requests.post(url, headers=headers, data=payload)
+    assert response.status_code == 200
+    return response.json()
+
+
+@pytest.fixture
+def get_body_create_customer(get_token_login):
+    token = get_token_login
+    url = Singleton.get_base_url() + "/rest/default/V1/customers"
+    payload = json.dumps({
+        "customer": {
+            "email": f"{Singleton.get_random_string(4)}@gmail.com",
+            "firstname": "jorge",
+            "lastname": "flores"
+        }
+    })
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {token}',
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    assert response.status_code == 200
+    return response.json()
