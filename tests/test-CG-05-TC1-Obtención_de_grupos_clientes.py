3aec7b877013b29786c26ec863afeee67f2ebbaf
Valeria220499 <20170027@est.umss.edu>
2024-06-28 20:13:50 -0400

    resultado exitoso de los grupos de clientes

--- tests/test-CG-05-TC1-Obtención_de_grupos_clientes.py
+++ tests/test-CG-05-TC1-Obtención_de_grupos_clientes.py
@@ -4,9 +4,9 @@ import pytest
 from src.singleton import Singleton
 
 
-def test_schema_customer_group_id(get_body_customer_group_id):
-    response_data = get_body_customer_group_id
-    schema = Singleton.read_schema_json_file('get_customer_group_id.json')
+def test_schema_customer_group_por_id(get_body_customer_group_por_id):
+    response_data = get_body_customer_group_por_id
+    schema = Singleton.read_schema_json_file('get_customer_group_por_id.json')
     try:
         jsonschema.validate(instance=response_data, schema=schema)
     except jsonschema.exceptions.ValidationError as err:
