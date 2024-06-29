68161cffd87cf5309348c3bd3de165cb4d69c5c5
Jorge Flores Chipana <201400892@est.umss.edu>
2024-06-29 10:07:53 -0400

    Refactorizacion de assertions de esquemas (Jorge)

--- tests/test_post_verify_create_customer.py
+++ tests/test_post_verify_create_customer.py
@@ -1,23 +1,11 @@
-import jsonschema
-import pytest
-
-from src.singleton import Singleton
+from src.assertions.assertions_schemas import assert_schemas
 
 
 def test_C2TC7_POST_validar_esquema_crear_nueva_cuenta_customer_solo_con_la_informacion_valida_requerida(
         get_body_of_create_a_customer_with_basic_information):
-    response_data = get_body_of_create_a_customer_with_basic_information
-    schema = Singleton.read_schema_json_file('post_create_customer.json')
-    try:
-        jsonschema.validate(instance=response_data, schema=schema)
-    except jsonschema.exceptions.ValidationError as err:
-        pytest.fail(f'JSON schema dont match {err}')
+    assert_schemas(get_body_of_create_a_customer_with_basic_information, 'post_create_customer.json')
 
 
-def test_C2TC8_POST_validar_esquema_crear_nueva_cuenta_customer_con_todo_el_esquema_llenado(get_body_of_create_a_customer_with_full_information):
-    response_data = get_body_of_create_a_customer_with_full_information
-    schema = Singleton.read_schema_json_file('post_create_customer_full_information.json')
-    try:
-        jsonschema.validate(instance=response_data, schema=schema)
-    except jsonschema.exceptions.ValidationError as err:
-        pytest.fail(f'JSON schema dont match {err}')
+def test_C2TC8_POST_validar_esquema_crear_nueva_cuenta_customer_con_todo_el_esquema_llenado(
+        get_body_of_create_a_customer_with_full_information):
+    assert_schemas(get_body_of_create_a_customer_with_full_information, 'post_create_customer_full_information.json')
