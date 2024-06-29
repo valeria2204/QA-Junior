84bcd6e9a83d2001da029cf0c09d5d568adc85cd
Valeria220499 <20170027@est.umss.edu>
2024-06-28 20:01:06 -0400

    resultado exitoso de los grupos de clientes

---
+++ tests/test-CG-05-TC1-Obtención_de_grupos_clientes.py
@@ -0,0 +1,14 @@
+import jsonschema
+import pytest
+
+from src.singleton import Singleton
+
+
+def test_schema_customer_group_id(get_body_customer_group_id):
+    response_data = get_body_customer_group_id
+    schema = Singleton.read_schema_json_file('get_customer_group_id.json')
+    try:
+        jsonschema.validate(instance=response_data, schema=schema)
+    except jsonschema.exceptions.ValidationError as err:
+        pytest.fail(f'JSON schema dont match {err}')
+
