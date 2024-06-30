847738ca4c9a7f2bc4b8760475e3ae7d478dca91
TATIANA MAYORGA ORELLANA <73003469+shizukani9@users.noreply.github.com>
2024-06-30 18:05:33 -0400

    CG6TC3,CG6TC4,CG6TC5,CG6TC6,CG6TC7 (#16)
    * archivo gitignore
    
    * archivo code postman
    
    * archivo test_CG_06_TC1_GET Caso de prueba 1: 200 Success ID válido de un grupo de clientes existente
    
    * CG-06-TC1: [GET]Verificar obtención exitosa de un grupo de clientes por ID
    
    * Caso de prueba 1: Verificar obtención exitosa de un grupo de clientes por ID
    
    * Validacion de esquema
    
    * Validacion de esquema probando con assert
    
    * esquema de get
    
    * esquema automatizado
    
    * actualizando test
    
    * cambios de esquema
    
    * refactorizacion de assertions para esquema
    
    * borrado de datos sensibles
    
    * eliminado de archivo esquema
    
    * remover credenciales
    
    * Renover cambios de login
    
    * Remover cambios de login
    
    * Remover assertions
    
    * Correccion de titulos de test
    
    * mejora de titulos de test
    
    * correcion de titulos test cases
    
    * correccion de librerias
    
    * test cases automatizados
    
    * Delete .gitignore
    
    * Delete src/assertions/asserions_schema.py
    
    * Delete src/json_files/schemas/get_customer_group.json
    
    * Fix de customer test

--- tests/test_CG_06_GET_Obtener_CustomerGroup_por_id.py
+++ tests/test_CG_06_GET_Obtener_CustomerGroup_por_id.py
@@ -1,6 +1,5 @@
 import pytest
 import requests
-import jsonschema
 
 from src.assertions.asserions_schema import assert_schemas
 from src.singleton import Singleton
@@ -21,8 +20,93 @@ def test_CG6TC1_GET_verificar_obtencion_exitosa_de_customer_group_por_id(get_tok
     response_data = response.json()
     assert response.status_code == 200
 
+    
 @pytest.mark.functional
-def test_CG6TC9_GET_validad_esquema_obtencion_exitosa_de_customer_group_por_id(get_body_of_obtain_customer_group_by_id):
+def test_CG6TC2_GET_validar_esquema_obtencion_exitosa_de_customer_group_por_id(get_body_of_obtain_customer_group_by_id):
     response_data = get_body_of_obtain_customer_group_by_id
     assert_schemas(response_data, 'get_customer_group.json')
 
+    
+@pytest.mark.smoke
+def test_CG6TC3_GET_verificar_respuesta_de_error_al_solicitar_un_customer_group_con_id_alfabetico(get_token_login):
+
+    group_id = "ASD"
+
+    token = get_token_login
+    url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/{group_id}"
+
+    payload = {}
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    response_data = response.json()
+    assert response.status_code == 400
+
+    
+@pytest.mark.functional
+def test_CG6TC4_GET_verificar_respuesta_de_error_al_solicitar_un_customer_group_con_id_de_caracter_especial(get_token_login):
+    group_id = "@"
+
+    token = get_token_login
+    url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/{group_id}"
+
+    payload = {}
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    response_data = response.json()
+    assert response.status_code == 400
+
+    
+@pytest.mark.functional
+def test_CG6TC5_GET_verificar_respuesta_de_error_al_solicitar_un_customer_group_con_id_vacio(get_token_login):
+    group_id = ""
+
+    token = get_token_login
+    url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/{group_id}"
+
+    payload = {}
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    response_data = response.json()
+    assert response.status_code == 404
+
+    
+@pytest.mark.functional
+def test_CG6TC6_GET_verificar_respuesta_de_error_al_solicitar_un_customer_group_con_id_no_existente(get_token_login):
+    group_id = 52542542542
+
+    token = get_token_login
+    url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/{group_id}"
+
+    payload = {}
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    response_data = response.json()
+    assert response.status_code == 404
+
+    
+def test_CG6TC7_GET_validar_error_de_no_autorizado_al_acceder_sin_token_de_autorizacion():
+    group_id = 1
+    token = Singleton.get_token_no_valid()
+    url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/{group_id}"
+
+    payload = {}
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    response_data = response.json()
+
+    assert response.status_code == 401
