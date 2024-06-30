1ed019880c7f7cb767fe95191f4eb393fcda951a
shizukani9 <tatianamayorga9999@gmail.com>
2024-06-29 23:10:44 -0400

    esquema automatizado

--- tests/conftest.py
+++ tests/conftest.py
@@ -79,17 +79,15 @@ def get_body_of_create_a_customer_with_full_information(get_token_login):
     assert response.status_code == 200
     return response.json()
 
+
 @pytest.fixture
-def get_body_customer_group(get_token_login):
-    token = get_token_login
-    url = Singleton.get_base_url() + "/rest/default/V1/customerGroups/default/"
+def get_body_of_obtain_customer_group_by_id(get_token_login, group_id=1):
+    url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/{group_id}"
     payload = {}
     headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer {token}',
+        'Authorization': f'Bearer {get_token_login}',
     }
 
-    response = requests.request("GET", url, headers=headers, data=payload)
+    response = requests.get(url, headers=headers, data=payload)
 
-    assert response.status_code == 200
     return response.json()
