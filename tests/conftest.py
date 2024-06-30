59828d5f4f7711cc23c1784080ee1402bb8833dc
shizukani9 <tatianamayorga9999@gmail.com>
2024-06-30 17:54:54 -0400

    Fix de customer test

--- tests/conftest.py
+++ tests/conftest.py
@@ -67,3 +67,65 @@ def get_body_of_obtain_customer_group_by_id(get_token_login, group_id=1):
     response = requests.get(url, headers=headers, data=payload)
 
     return response.json()
+
+
+@pytest.fixture
+def get_body_of_create_a_customer_with_full_information(get_token_login):
+    token = get_token_login
+    url = Singleton.get_base_url() + "/rest/default/V1/customers"
+    random_alphanumeric_value = Singleton.get_random_alphanumeric(4)
+    payload = json.dumps({
+        "customer": {
+            "group_id": 1,
+            "default_billing": "2024-04-07 23:49:59",
+            "default_shipping": "2024-04-07 23:49:59",
+            "created_at": "2024-04-07 23:49:59",
+            "updated_at": "2024-04-07 23:49:59",
+            "created_in": "Default Store View",
+            "dob": "2024-04-17",
+            "email": f"{random_alphanumeric_value}@gmail.com",
+            "firstname": f"{random_alphanumeric_value}_FN",
+            "lastname": f"{random_alphanumeric_value}_LN",
+            "middlename": f"{random_alphanumeric_value}_MN",
+            "prefix": f"{random_alphanumeric_value}_P",
+            "suffix": f"{random_alphanumeric_value}_S",
+            "gender": 1,
+            "store_id": 1,
+            "website_id": 1,
+            "addresses": [],
+            "disable_auto_group_change": 0},
+        "password": f"{Singleton.get_password()}!!",
+        "redirectUrl": "string"
+    })
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {token}',
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    assert response.status_code == 200
+    return response.json()
+
+
+@pytest.fixture
+def get_body_of_create_a_customer_with_basic_information(get_token_login):
+    token = get_token_login
+    url = Singleton.get_base_url() + "/rest/default/V1/customers"
+    payload = json.dumps({
+        "customer": {
+            "email": f"{Singleton.get_random_alphanumeric(4)}@gmail.com",
+            "firstname": "jorge",
+            "lastname": "flores"
+        }
+    })
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {token}',
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    assert response.status_code == 200
+    return response.json()
