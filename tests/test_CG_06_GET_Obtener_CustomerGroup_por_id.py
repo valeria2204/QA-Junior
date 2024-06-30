e1d3f125090e92ef66c821c51e33fd688474da82
shizukani9 <tatianamayorga9999@gmail.com>
2024-06-29 23:21:19 -0400

    cambios de esquema

--- tests/test_CG_06_GET_Obtener_CustomerGroup_por_id.py
+++ tests/test_CG_06_GET_Obtener_CustomerGroup_por_id.py
@@ -6,11 +6,31 @@ from src.singleton import Singleton
 
 # CG-06-TC1: [GET] Verificar obtención exitosa de un grupo de clientes por ID
 @pytest.mark.smoke
-def test_CG_06_TC1_GET_verificar_obtencion_exitosa_de_un_grupo_de_clientes_por_id(get_token_login):
+def test_CG_06_TC1_GET_verificar_obtencion_exitosa_de_un_customer_group_por_id(get_token_login):
     group_id = 1  # ID válido de un grupo de clientes existente
 
     token = get_token_login
     url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/{group_id}"
 
     headers = {
-        'Authorization':
+        'Authorization': f'Bearer {token}',
+    }
+
+    response = requests.get(url, headers=headers)
+    response_data = response.json()
+
+    assert response.status_code == 200
+    assert "id" in response_data
+    assert response_data["id"] == group_id
+    assert "code" in response_data
+    assert "tax_class_id" in response_data
+    assert "tax_class_name" in response_data
+
+def test_esquema_verificar_obtencion_exitosa_de_un_customer_group_por_id(get_body_of_obtain_customer_group_by_id):
+    response_data = get_body_of_obtain_customer_group_by_id
+    schema = Singleton.read_schema_json_file('get_customer_group.json')
+    try:
+        jsonschema.validate(instance=response_data, schema=schema)
+    except jsonschema.exceptions.ValidationError as err:
+        pytest.fail(f'JSON schema dont match {err}')
+
