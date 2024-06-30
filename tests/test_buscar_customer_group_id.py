e41532b18a0a644bc667a628388b8be187041a89
Valeria220499 <20170027@est.umss.edu>
2024-06-30 00:38:46 -0400

    casos de prueba para el endpoint GET-Buscar Customer Group por ID

---
+++ tests/test_buscar_customer_group_id.py
@@ -0,0 +1,111 @@
+import pytest
+import requests
+
+from src.singleton import Singleton
+
+
+@pytest.mark.functional
+def test_TC4_POST_verificar_codigo_de_error_en_endpoint(get_token_login):
+    token = get_token_login
+    url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/search"
+
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json',
+    }
+
+    payload = {}
+
+    response = requests.post(url, headers=headers, json=payload)
+    response_data = response.json()
+
+    assert response.status_code == 404, "NO FOUND 404"
+
+
+@pytest.mark.functional
+def test_TC5_PUT_Verificar_codigo_de_error_en_endpoint(get_token_login):
+    token = get_token_login
+    url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/search"
+
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json',
+    }
+
+    payload = {}
+
+    response = requests.put(url, headers=headers, json=payload)
+    response_data = response.json()
+
+    assert response.status_code == 400, "NO FOUND 400"
+
+
+@pytest.mark.functional
+def test_TC6_DELETE__Verificar_codigo_de_error_en_endpoint(get_token_login):
+    token = get_token_login
+    url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/search"
+
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json',
+    }
+
+    payload = {}
+
+    response = requests.delete(url, headers=headers, json=payload)
+    response_data = response.json()
+
+    assert response.status_code == 400, "NO FOUND 400"
+
+@pytest.mark.regression
+def test_TC4_POST_verificar_codigo_de_error_en_endpoint(get_token_login):
+    token = get_token_login
+    url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/search"
+
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json',
+    }
+
+    payload = {}
+
+    response = requests.post(url, headers=headers, json=payload)
+    response_data = response.json()
+
+    assert response.status_code == 404, "NO FOUND 404"
+
+
+@pytest.mark.regression
+def test_TC5_PUT_Verificar_codigo_de_error_en_endpoint(get_token_login):
+    token = get_token_login
+    url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/search"
+
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json',
+    }
+
+    payload = {}
+
+    response = requests.put(url, headers=headers, json=payload)
+    response_data = response.json()
+
+    assert response.status_code == 400, "NO FOUND 400"
+
+
+@pytest.mark.regression
+def test_TC6_DELETE__Verificar_codigo_de_error_en_endpoint(get_token_login):
+    token = get_token_login
+    url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/search"
+
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json',
+    }
+
+    payload = {}
+
+    response = requests.delete(url, headers=headers, json=payload)
+    response_data = response.json()
+
+    assert response.status_code == 400, "NO FOUND 400"
---
+++ tests/test_get_buscar_Customer_group_id.py
@@ -0,0 +1,67 @@
+import pytest
+import requests
+
+from src.singleton import Singleton
+
+
+@pytest.mark.smoke
+def test_TC11_verificar_respuesta_exitosa(get_token_login):
+    token = get_token_login
+    url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/search"
+
+    params = {
+        'searchCriteria[filterGroups][0][filters][0][field]': 'id',
+        'searchCriteria[filterGroups][0][filters][0][value]': '1',
+        'searchCriteria[filterGroups][0][filters][0][condition_type]': 'eq'
+    }
+
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    response_data = response.json()
+
+    assert response.status_code == 200, "Expected status code 200"
+
+
+@pytest.mark.smoke
+def test_TC3_verificar_comportamiento_con_parametros_invalidos(get_token_login):
+    token = get_token_login
+    url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/search"
+
+    params = {
+        'searchCriteria[filterGroups][0][filters][0][field]': 'nonexistent_field',
+        'searchCriteria[filterGroups][0][filters][0][value]': 'invalid_value',
+        'searchCriteria[filterGroups][0][filters][0][condition_type]': 'invalid_condition'
+    }
+
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    response_data = response.json()
+
+    assert response.status_code == 500, "Internal Server Error"
+
+
+@pytest.mark.regression
+def test_TC7_verificar_mensaje_de_exito(get_token_login):
+    token = get_token_login
+    url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/search"
+
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+
+    params = {
+        'searchCriteria[filterGroups][0][filters][0][field]': 'id',
+        'searchCriteria[filterGroups][0][filters][0][value]': '1',
+        'searchCriteria[filterGroups][0][filters][0][condition_type]': 'eq'
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    response_data = response.json()
+
+    assert response.status_code == 200, "Expected status code 200"
