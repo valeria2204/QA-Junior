3ce571734cae04df24993d06801f487723f5ea4e
Neyva Soto Sierra <201110806@est.umss.edu>
2024-06-30 23:31:36 -0400

    CG-02 Renombrar nombre de los test cases

--- tests/test_get_customer_group_default.py
+++ tests/test_get_customer_group_default.py
@@ -5,7 +5,7 @@ from src.assertions.asserions_schema import assert_schemas
 from src.singleton import Singleton
 
 
-def test_CG2TC1_GET_verificar_la_obtencion_exitosa_de_CustomerGroup_por_defecto(get_token_login):
+def test_CG2TC1_GET_verificar_la_obtencion_exitosa_de_customer_groups_por_defecto(get_token_login):
     url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/default"
 
     payload = {}
@@ -18,7 +18,7 @@ def test_CG2TC1_GET_verificar_la_obtencion_exitosa_de_CustomerGroup_por_defecto(
     assert response.status_code == 200
 
 
-def test_CG2TC2_GET_verificar_esquema_obtencion_exitosa_de_CustomerGroup_por_defecto(get_token_login):
+def test_CG2TC2_GET_verificar_esquema_obtencion_exitosa_de_customer_groups_por_defecto(get_token_login):
     url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/default"
 
     payload = {}
@@ -32,7 +32,7 @@ def test_CG2TC2_GET_verificar_esquema_obtencion_exitosa_de_CustomerGroup_por_def
     assert_schemas(response.json(), 'get_customer_group.json')
 
 
-def test_CG2TC3_GET_validar_respuesta_401_al_enviar_un_token_invalido(get_token_login):
+def test_CG2TC3_GET_verificar_que_retorna_una_respuesta_401_al_obtener_el_customer_groups_por_defecto_cuando_no_tiene_un_token_valido(get_token_login):
     url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/default"
 
     payload = {}
@@ -45,7 +45,7 @@ def test_CG2TC3_GET_validar_respuesta_401_al_enviar_un_token_invalido(get_token_
     assert response.status_code == 401
 
 
-def test_CG2TC4_GET_validar_respuesta_401_al_enviar_la_solicitud_sin_el_header_Authorization(get_token_login):
+def test_CG2TC4_GET_verificar_que_retorna_una_respuesta_401_al_obtener_el_customer_groups_por_defecto_cuando_no_tiene_un_header_authorization(get_token_login):
     url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/default"
 
     payload = {}
@@ -56,7 +56,7 @@ def test_CG2TC4_GET_validar_respuesta_401_al_enviar_la_solicitud_sin_el_header_A
     assert response.status_code == 401
 
 
-def test_CG2TC5_GET_verificar_que_la_respuesta_tenga_un_id_numero_entero_positivo(get_token_login):
+def test_CG2TC5_GET_verificar_la_obtencion_de_el_customer_groups_por_defecto_tenga_el_id_de_tipo_numero_entero_positivo(get_token_login):
     url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/default"
 
     payload = {}
@@ -72,7 +72,7 @@ def test_CG2TC5_GET_verificar_que_la_respuesta_tenga_un_id_numero_entero_positiv
     assert response_data["id"] >= 0
 
 
-def test_CG2TC6_GET_verificar_que_la_respuesta_tenga_un_class_id_numero_entero_positivo(get_token_login):
+def test_CG2TC6_GET_verificar_la_obtencion_de_el_customer_groups_por_defecto_tenga_el_class_id_de_tipo_numero_entero_positivo(get_token_login):
     url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/default"
 
     payload = {}
@@ -88,7 +88,7 @@ def test_CG2TC6_GET_verificar_que_la_respuesta_tenga_un_class_id_numero_entero_p
     assert response_data["tax_class_id"] >= 0
 
 
-def test_CG2TC7_GET_verificar_que_la_respuesta_tenga_un_code_cadena_no_vacia(get_token_login):
+def test_CG2TC7_GET_verificar_la_obtencion_de_el_customer_groups_por_defecto_tenga_el_valor_de_code_no_vacio(get_token_login):
     url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/default"
 
     payload = {}
@@ -104,7 +104,7 @@ def test_CG2TC7_GET_verificar_que_la_respuesta_tenga_un_code_cadena_no_vacia(get
     assert len(response_data["code"]) >= 0
 
 
-def test_CG2TC8_GET_verificar_que_la_respuesta_tenga_un_tax_class_name_cadena_no_vacia(get_token_login):
+def test_CG2TC8_GET_verificar_la_obtencion_de_el_customer_groups_por_defecto_tenga_el_valor_de_tax_class_name_no_vacio(get_token_login):
     url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/default"
 
     payload = {}
@@ -120,7 +120,7 @@ def test_CG2TC8_GET_verificar_que_la_respuesta_tenga_un_tax_class_name_cadena_no
     assert len(response_data["tax_class_name"]) >= 0
 
 
-def test_CG2TC9_GET_verificar_que_el_resultado_no_se_ve_afectado_cuando_se_envia_un_parametro_de_consulta(get_token_login):
+def test_CG2TC9_GET_verificar_la_obtencion_de_el_customer_groups_por_defecto_no_se_ve_afectado_cuando_se_envia_un_parametro_de_consulta(get_token_login):
     url = f"{Singleton.get_base_url()}/rest/default/V1/customerGroups/default?code=LoremIpsum"
 
     payload = {}
