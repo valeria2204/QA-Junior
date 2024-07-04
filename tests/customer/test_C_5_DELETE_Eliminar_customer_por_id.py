709c94e05ca9106cd8690d423b3656688c4a5598
shizukani9 <tatianamayorga9999@gmail.com>
2024-07-04 15:29:18 -0400

    creacion de archivo

---
+++ tests/customer/test_C_5_DELETE_Eliminar_customer_por_id.py
@@ -0,0 +1,8 @@
+import pytest
+
+from src.assertions.assertions_schema import assert_schemas
+from src.testdata import TestData
+from tests.conftest import send_request_of_create_a_customer
+from tests.conftest import setup_data
+from tests.helpers.utils import Utils
+
