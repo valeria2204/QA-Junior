46f3e51baec5ba0c1667c59b014f8a17a13e1800
shizukani9 <tatianamayorga9999@gmail.com>
2024-07-04 22:43:37 -0400

    cambios

--- tests/customer/test_C_5_DELETE_Eliminar_customer_por_id.py
+++ tests/customer/test_C_5_DELETE_Eliminar_customer_por_id.py
@@ -6,3 +6,16 @@ from tests.conftest import send_request_of_create_a_customer
 from tests.conftest import setup_data
 from tests.helpers.utils import Utils
 
+
+def test_delete_existing_customer(setup_data):
+    # Crear un nuevo cliente con un email único
+    email = "test@example.com"
+    firstname = "Test"
+    lastname = "User"
+    send_request_of_create_a_customer(email, firstname, lastname)
+
+    # Validar que la creación fue exitosa
+    assert TestData.response_status_code == 200
+
+
+
