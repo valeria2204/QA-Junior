865d1f073d152d44db360e280b7ef00fae0a998b
shizukani9 <tatianamayorga9999@gmail.com>
2024-07-05 23:13:19 -0400

    delete customer

--- tests/customer/test_C_5_DELETE_Eliminar_customer_por_id.py
+++ tests/customer/test_C_5_DELETE_Eliminar_customer_por_id.py
@@ -1,19 +1,18 @@
 import pytest
 
+from src.assertions.assertions import assert_response_status
 from src.assertions.assertions_schema import assert_schemas
 from src.testdata import TestData
-from tests.conftest import send_request_of_create_a_customer
+from tests.customer.setup import setup_module, send_request_of_create_a_customer, setup_function, \
+    send_request_of_remove_customer
+
 from tests.conftest import setup_data
-from tests.helpers.utils import Utils
 
+@pytest.mark.smoke
+@pytest.mark.functional
+@pytest.mark.regression
+def test_delete_existing_customer(setup_function):
+    send_request_of_create_a_customer("tati8@gmail.com", "Tati", "Mayo")
+    assert_response_status(TestData.response_status_code, 200)
 
-def test_delete_existing_customer(setup_data):
-#Crear customer
-    email = "tati@gmail.com"
-    firstname = "tati"
-    lastname = "tati"
-    send_request_of_create_a_customer(email, firstname, lastname)
-    assert TestData.response_status_code == 200
-
-#elminar cliente
 
