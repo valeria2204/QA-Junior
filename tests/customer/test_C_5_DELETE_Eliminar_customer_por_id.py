6f5125ce61e902537de82aceba9a5e58cc66f8a0
shizukani9 <tatianamayorga9999@gmail.com>
2024-07-06 12:39:36 -0400

    cambios

--- tests/customer/test_C_5_DELETE_Eliminar_customer_por_id.py
+++ tests/customer/test_C_5_DELETE_Eliminar_customer_por_id.py
@@ -11,8 +11,15 @@ from tests.conftest import setup_data
 @pytest.mark.smoke
 @pytest.mark.functional
 @pytest.mark.regression
-def test_delete_existing_customer(setup_function):
+def test_delete_existing_customer_NUEVO(setup_function):
     send_request_of_create_a_customer("tati8@gmail.com", "Tati", "Mayo")
     assert_response_status(TestData.response_status_code, 200)
 
 
+@pytest.mark.smoke
+@pytest.mark.functional
+@pytest.mark.regression
+def test_delete_existing_customer_ID_INVALIDO(setup_function):
+    customer_id = "#"
+    send_request_of_remove_customer(customer_id)
+    assert_response_status(TestData.response_status_code, 404)
