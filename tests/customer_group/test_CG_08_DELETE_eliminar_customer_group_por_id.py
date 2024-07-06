bb17e9af39f2b76d8dda21062ec9a1318df47704
samuR57 <201704865@est.umss.edu>
2024-07-06 19:17:48 -0400

    Cambios realizados

--- tests/customer_group/test_CG_08_DELETE_eliminar_customer_group_por_id.py
+++ tests/customer_group/test_CG_08_DELETE_eliminar_customer_group_por_id.py
@@ -4,7 +4,6 @@ import requests
 from src.assertions.assertions import assert_response_status
 from src.assertions.assertions_schema import assert_schemas
 from src.testdata import TestData
-from tests.customer_group.setup import send_request_of_delete_customer_group_by_id
 from tests.conftest import setup_data
 from tests.customer_group.setup import setup_function
 from tests.customer_group.setup import send_request_of_remove_customer_group
@@ -18,57 +17,57 @@ from tests.helpers.utils import Utils
 @pytest.mark.smoke
 @pytest.mark.functional
 def test_CG8TC1_DELETE_verificar_la_eliminacion_de_un_customer_group_con_id_valido(setup_function):
-    send_request_of_delete_customer_group_by_id(TestData.function_response_json["id"])
+    send_request_of_remove_customer_group(TestData.function_response_json["id"])
     assert_response_status(TestData.response_status_code, 200)
 
 
 @pytest.mark.smoke
 @pytest.mark.functional
 def test_CG8TC2_DELETE_verificar_la_eliminacion_de_un_customer_group_con_id_de_un_grupo_predeterminado(setup_data):
-    send_request_of_delete_customer_group_by_id(StaticData.group_id.value)
+    send_request_of_remove_customer_group(StaticData.group_id.value)
     assert_response_status(TestData.response_status_code, 400)
 
 
 @pytest.mark.smoke
 @pytest.mark.functional
 def test_CG8TC3_DELETE_verificar_la_eliminacion_de_un_customer_group_sin_token_de_autenticacion_con_id_valido(setup_function):
-    send_request_of_delete_customer_group_by_id(StaticData.group_id.value, StaticData.empty_name.value)
+    send_request_of_remove_customer_group(StaticData.group_id.value, StaticData.empty_name.value)
     assert_response_status(TestData.response_status_code, 401)
 
 
 @pytest.mark.smoke
 @pytest.mark.functional
 def test_CG8TC4_DELETE_verificar_la_eliminacion_de_un_customer_group_que_no_existe(setup_function):
-    send_request_of_delete_customer_group_by_id(StaticData.non_existing_id.value)
+    send_request_of_remove_customer_group(StaticData.non_existing_id.value)
     assert_response_status(TestData.response_status_code, 404)
 
 
 @pytest.mark.functional
 def test_CG8TC5_DELETE_verificar_status_code_OK_de_un_customer_group_utilizando_metodo_GET_en_lugar_de_DELETE(setup_function):
-    send_request_of_delete_customer_group_by_id(StaticData.group_id.value,None, Method.GET.value)
+    send_request_of_remove_customer_group(StaticData.group_id.value,None, Method.GET.value)
     assert_response_status(TestData.response_status_code, 200)
 
 
 @pytest.mark.functional
 def test_CG8TC6_DELETE_verificar_la_eliminacion_de_un_customer_group_introduciendo_texto_en_lugar_de_id(setup_function):
-    send_request_of_delete_customer_group_by_id(StaticData.firstname.value)
+    send_request_of_remove_customer_group(StaticData.firstname.value)
     assert_response_status(TestData.response_status_code, 400)
 
 @pytest.mark.regression
 @pytest.mark.functional
 def test_CG8TC7_DELETE_verificar_la_eliminacion_de_un_customer_group_con_simbolos_en_lugar_de_id(setup_function):
     simbolos = Utils().get_random_symbols(5)
-    send_request_of_delete_customer_group_by_id(simbolos, None)
+    send_request_of_remove_customer_group(simbolos, None)
     assert_response_status(TestData.response_status_code, 400)
 
 
 @pytest.mark.functional
 def test_CG8TC8_DELETE_verificar_la_eliminacion_de_un_customer_group_con_id_vacio(setup_function):
-    send_request_of_delete_customer_group_by_id(StaticData.empty_name.value)
+    send_request_of_remove_customer_group(StaticData.empty_name.value)
     assert_response_status(TestData.response_status_code, 404)
 
 
 @pytest.mark.regression
 def test_CG8TC9_DELETE_verificar_status_code_de_un_customer_group_utilizando_metodo_POST_en_lugar_de_DELETE(setup_function):
-    send_request_of_delete_customer_group_by_id(StaticData.group_id.value, None, Method.POST.value)
+    send_request_of_remove_customer_group(StaticData.group_id.value, None, Method.POST.value)
     assert_response_status(TestData.response_status_code, 404)
