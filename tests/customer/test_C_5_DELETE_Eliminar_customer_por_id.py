55f519d1f0d94c5de0a6d0d1fb7cfc900891d80f
shizukani9 <tatianamayorga9999@gmail.com>
2024-07-06 18:32:31 -0400

    cambios 18_32

--- tests/customer/test_C_5_DELETE_Eliminar_customer_por_id.py
+++ tests/customer/test_C_5_DELETE_Eliminar_customer_por_id.py
@@ -1,11 +1,12 @@
 import pytest
 
-from src.assertions.assertions import assert_response_status
+from src.assertions.assertions import assert_response_status, assert_equals
 from src.assertions.assertions_schema import assert_schemas
+from src.enums.schema_json_name import SchemaName
 from src.enums.static_data import StaticData
 from src.testdata import TestData
 from tests.customer.setup import setup_module, send_request_of_create_a_customer, setup_function, \
-    send_request_of_remove_customer, send_request_of_create_full_customer_with_params, setup_function_request_of_create_full_customer_with_params
+    send_request_of_remove_customer, setup_function_full_customer, setup_function_full_customer
 
 from tests.conftest import setup_data
 from tests.helpers.utils import Utils
@@ -15,10 +16,7 @@ from tests.helpers.utils import Utils
 @pytest.mark.functional
 @pytest.mark.regression
 def test_C5TC1_DELETE_verificar_status_code_200_al_eliminar_un_customer_nuevo(setup_function):
-    email = Utils.get_random_email()
-    firstname = StaticData.firstname.value
-    lastname = StaticData.lastname.value
-    send_request_of_create_a_customer(email, firstname, lastname)
+    send_request_of_remove_customer(TestData.function_response_json["id"])
     assert_response_status(TestData.response_status_code, 200)
 
 
@@ -26,7 +24,7 @@ def test_C5TC1_DELETE_verificar_status_code_200_al_eliminar_un_customer_nuevo(se
 @pytest.mark.functional
 @pytest.mark.regression
 def test_C5TC2_DELETE_verificar_status_code_400_al_eliminar_un_customer_con_customer_id_invalido_de_tipo_simbolo(setup_function):
-    customer_id = Utils.get_random_symbols(1)
+    customer_id = Utils.get_random_symbols(5)
     send_request_of_remove_customer(customer_id)
     assert_response_status(TestData.response_status_code, 400)
 
@@ -44,7 +42,7 @@ def test_C5TC3_DELETE_verificar_status_code_400_al_eliminar_un_customer_con_cust
 @pytest.mark.functional
 @pytest.mark.regression
 def test_C5TC4_DELETE_verificar_status_code_404_al_eliminar_un_customer_con_customer_id_vacio(setup_function):
-    customer_id = ""
+    customer_id = StaticData.empty_name.value
     send_request_of_remove_customer(customer_id)
     assert_response_status(TestData.response_status_code, 404)
 
@@ -62,34 +60,44 @@ def test_C5TC5_DELETE_verificar_status_code_404_al_eliminar_un_customer_con_cust
 @pytest.mark.functional
 @pytest.mark.regression
 def test_C5TC6_DELETE_verificar_status_code_200_al_eliminar_un_customer_con_los_requerimientos_minimos(setup_function):
-    email = Utils.get_random_email()
-    firstname = StaticData.firstname.value
-    lastname = StaticData.lastname.value
-    send_request_of_create_a_customer(email, firstname, lastname)
+    send_request_of_remove_customer(TestData.function_response_json["id"])
     assert_response_status(TestData.response_status_code, 200)
 
 
 @pytest.mark.smoke
 @pytest.mark.functional
 @pytest.mark.regression
-def test_C5TC7_DELETE_verificar_status_code_200_al_eliminar_un_customer_con_todos_los_valores_llenados():
-    test_data = setup_function_request_of_create_full_customer_with_params()
-    assert_response_status(test_data.response_status_code, 200)
-    send_request_of_remove_customer(test_data.full_customer_response_json["id"])
+def test_C5TC7_DELETE_verificar_status_code_200_al_eliminar_un_customer_con_todos_los_valores_llenados(setup_function_full_customer):
+    assert_response_status(TestData.response_status_code, 200)
 
 
 @pytest.mark.functional
 @pytest.mark.regression
 def test_CG6TC8_GET_verificar_status_code_401_al_eliminar_un_customer_sin_token_de_autorizacion(setup_function):
-    email = Utils.get_random_email()
-    firstname = StaticData.firstname.value
-    lastname = StaticData.lastname.value
-    send_request_of_create_a_customer(email, firstname, lastname)
+    send_request_of_remove_customer(TestData.function_response_json["id"], TestData.token_no_valid)
     assert_response_status(TestData.response_status_code, 401)
-    #no funciona
+
+
+@pytest.mark.smoke
+@pytest.mark.functional
+@pytest.mark.regression
+def test_C5TC9_DELETE_validar_respuesta_true_al_eliminar_un_customer_nuevo(setup_function):
+    response = send_request_of_remove_customer(TestData.function_response_json["id"])
+    assert_response_status(TestData.response_status_code, 200)
+    assert_equals(response, True)
 
 
 @pytest.mark.functional
 @pytest.mark.regression
-def test_C5TC9_DELETE_validar_esquema_status_code_200_al_eliminar_un_customer_nuevo(setup_module)
-    assert_schemas(TestData.module_response_json, 'post_create_customer.json')
+def test_C5TC10_DELETE_Validar_esquema_status_code_400_al_eliminar_un_customer_con_customer_id_invalido_de_tipo_simbolo(setup_function):
+    customer_id = Utils.get_random_symbols(5)
+    response = send_request_of_remove_customer(customer_id)
+    assert_schemas(response, SchemaName.response_status_code_400_type_value_is_invalid.value)
+
+
+@pytest.mark.functional
+@pytest.mark.regression
+def test_C5TC_DELETE_validar_esquema_status_code_400_al_eliminar_un_customer_nuevo(setup_function):
+    customer_id = Utils.get_random_numerics(10)
+    response = send_request_of_remove_customer(customer_id)
+    assert_schemas(response, SchemaName.response_status_code_400_type_value_is_invalid.value)
