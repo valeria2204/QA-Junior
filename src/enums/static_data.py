bf9b6bdf03a9b8a20400002df030ae2b03b893e6
Jorge Flores Chipana <201400892@est.umss.edu>
2024-07-06 12:32:34 -0400

    Code refactoring of hardcode values: v1 completed

---
+++ src/enums/static_data.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+
+class StaticData(Enum):
+    empty_name = ""
+    customer = {}
+    email = "@gmail.com"
+    firstname = "Test_FirstName"
+    lastname = "Test_LastName"
+    group_id = 1
+    default_billing = "2024-04-07 23:49:59"
+    default_shipping = "2024-03-07 23:49:59"
+    created_at = "2024-01-07 23:49:59"
+    updated_at = "2024-07-06 04:58:18"
+    created_in = "Default Store View"
+    dob = "2020-04-17"
+    middlename = "MN"
+    prefix = "P"
+    suffix = "S"
+    gender = 1
+    store_id = 1
+    website_id = 1
+    addresses = []
+    disable_auto_group_change = 0
+    password = "Demo123#"
+    redirectUrl = "string"
+    non_existing_id = 99999
--- tests/helpers/utils.py
+++ tests/helpers/utils.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import os
 import random
@@ -16,4 +17,47 @@ class Utils:
     def get_random_alphanumeric(length):
         characters = string.ascii_letters + string.digits
         return "".join(random.choices(characters, k=length))
-    
+
+
+    @staticmethod
+    def get_random_email():
+        characters = string.ascii_letters + string.digits
+        return "".join(random.choices(characters, k=6))+"@gmail.com"
+
+
+    @staticmethod
+    def get_random_numerics(length):
+        return "".join(random.choices(string.digits, k=length))
+
+
+    @staticmethod
+    def get_random_letters(length):
+        return "".join(random.choices(string.ascii_letters, k=length))
+
+
+    @staticmethod
+    def get_random_symbols(length):
+        return "".join(random.choices(string.punctuation, k=length))
+
+
+    @staticmethod
+    def random_past_date(start=datetime.datetime(2000, 1, 1), end=datetime.datetime.now()):
+        return start + datetime.timedelta(seconds=random.randint(0, int((end - start).total_seconds())))
+
+
+    @staticmethod
+    def random_future_date(start=datetime.datetime.now(), end=datetime.datetime(3000, 1, 1)):
+        return start + datetime.timedelta(seconds=random.randint(0, int((end - start).total_seconds())))
+
+
+    @staticmethod
+    def random_current_date():
+        return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+    @staticmethod
+    def get_random_password(length):
+        characters = string.ascii_letters + string.digits + string.punctuation
+        password = random.choices(characters, k=length)
+        return "".join(password)
+
