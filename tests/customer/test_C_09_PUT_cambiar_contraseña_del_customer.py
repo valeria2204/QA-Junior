0fb3a24b51a546ae2a3acf47572598042abfdae1
Valeria220499 <20170027@est.umss.edu>
2024-07-06 20:52:04 -0400

    modificado con las actualizaciones

--- tests/customer/test_C_09_PUT_cambiar_contraseña_del_customer.py
+++ tests/customer/test_C_09_PUT_cambiar_contraseña_del_customer.py
@@ -3,7 +3,6 @@ import pytest
 from src.assertions.assertions import assert_response_status
 from src.assertions.assertions_schema import assert_schemas
 from src.testdata import TestData
-
 from tests.customer.setup import send_request_of_update_password_of_a_customer, setup_module_customer_with_account
 from tests.helpers.utils import Utils
 from src.enums.static_data import StaticData
@@ -12,7 +11,7 @@ from src.enums.static_data import StaticData
 @pytest.mark.smoke
 @pytest.mark.functional
 @pytest.mark.regression
-def test_C9TC1_PUT_Verificar_status_code_400_cuando_se_cambia_password_de_customer_con_el_nuevo_password_es_tipo_alfabetico(setup_module_customer_with_account):
+def test_C9TC1_PUT_verificar_status_code_400_cuando_se_cambia_password_de_customer_con_el_nuevo_password_es_tipo_alfabetico(setup_module_customer_with_account):
     send_request_of_update_password_of_a_customer(TestData.module_response_json["id"], Utils.get_random_password(12), TestData.old_password)
     assert_response_status(TestData.response_status_code, 400)
 
@@ -20,7 +19,7 @@ def test_C9TC1_PUT_Verificar_status_code_400_cuando_se_cambia_password_de_custom
 @pytest.mark.smoke
 @pytest.mark.functional
 @pytest.mark.regression
-def test_C9TC2_PUT_Validar_esquema_status_code_400_cuando_se_cambia_password_de_customer_con_un_nuevo_password_de_tipo_alfabetico(setup_module_customer_with_account):
+def test_C9TC2_PUT_validar_esquema_status_code_400_cuando_se_cambia_password_de_customer_con_un_nuevo_password_de_tipo_alfabetico(setup_module_customer_with_account):
     response_json = send_request_of_update_password_of_a_customer(TestData.module_response_json["id"], Utils.get_random_password(12), TestData.old_password)
     assert_schemas(response_json, 'put_new_password_to_customer.json')
 
@@ -28,7 +27,7 @@ def test_C9TC2_PUT_Validar_esquema_status_code_400_cuando_se_cambia_password_de_
 @pytest.mark.smoke
 @pytest.mark.functional
 @pytest.mark.regression
-def test_C9TC3_PUT_Verificar_status_code_401_cuando_el_cambio_de_password_para_el_parametro_old_passsword_no_es_valido(setup_module_customer_with_account):
+def test_C9TC3_PUT_verificar_status_code_401_cuando_el_cambio_de_password_para_el_parametro_old_passsword_no_es_valido(setup_module_customer_with_account):
     send_request_of_update_password_of_a_customer(TestData.module_response_json["id"], Utils.get_random_password(12), old_password=Utils.get_random_letters(12))
     assert_response_status(TestData.response_status_code, 401)
 
@@ -36,7 +35,7 @@ def test_C9TC3_PUT_Verificar_status_code_401_cuando_el_cambio_de_password_para_e
 @pytest.mark.smoke
 @pytest.mark.functional
 @pytest.mark.regression
-def test_C9TC4_PUT_Validar_esquema_status_code_401_cuando_el_cambio_de_password_para_el_parametro_old_password_no_es_valido(setup_module_customer_with_account):
+def test_C9TC4_PUT_validar_esquema_status_code_401_cuando_el_cambio_de_password_para_el_parametro_old_password_no_es_valido(setup_module_customer_with_account):
     response_json = send_request_of_update_password_of_a_customer(TestData.module_response_json["id"], Utils.get_random_password(12), old_password=Utils.get_random_letters(12))
     assert_schemas(response_json, 'put_new_password_to_customer.json')
 
@@ -102,7 +101,7 @@ def test_C9TC11_PUT_verificar_status_code_400_cuando_el_cambio_de_password_de_cu
 @pytest.mark.smoke
 @pytest.mark.functional
 @pytest.mark.regression
-def test_C9TC12_PUT_Verificar_status_code_400_cuando_cuando_se_cambia_password_de_customer_con_el_id_incorrecto(setup_module_customer_with_account):
+def test_C9TC12_PUT_verificar_status_code_400_cuando_cuando_se_cambia_password_de_customer_con_el_id_incorrecto(setup_module_customer_with_account):
     send_request_of_update_password_of_a_customer(StaticData.empty_name, Utils.get_random_password(12), StaticData.password.value)
     assert_response_status(TestData.response_status_code, 400)
 
@@ -110,6 +109,6 @@ def test_C9TC12_PUT_Verificar_status_code_400_cuando_cuando_se_cambia_password_d
 @pytest.mark.smoke
 @pytest.mark.functional
 @pytest.mark.regression
-def test_C9TC13_PUT_Verificar_status_code_200_cuando_se_cambia_password_de_customer_con_valores_validos(setup_module_customer_with_account):
+def test_C9TC13_PUT_verificar_status_code_200_cuando_se_cambia_password_de_customer_con_valores_validos(setup_module_customer_with_account):
     send_request_of_update_password_of_a_customer(TestData.module_response_json["id"], Utils.get_random_password(12), StaticData.password.value)
     assert_response_status(TestData.response_status_code, 200)
